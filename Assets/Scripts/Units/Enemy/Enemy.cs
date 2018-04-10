44fa69d07cfd12e911b17314b5d1612ec32e8d99
mzk <kmzw1994@outlook.com>
2018-04-10 21:28:08 +0800

    Add Enemy
    20180410

---
+++ Assets/Scripts/Units/Enemy/Enemy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Units.Enemy
+{
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class Enemy : Targetable
+    {
+
+
+    }
+
+}
