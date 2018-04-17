616cc571d225a7fff80b0356ef33259d8e67dce2
kmzw1994 <kmzw1994@outlook.com>
2018-04-17 16:59:03 +0800

    Continue Wave
    20180417
    
    --+RepeatingTimer
    -- Wave

---
+++ Assets/Scripts/Core/Utilities/RepeatingTimer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Core.Utilities
+{
+    /// <summary>
+    /// 重复计时器
+    /// </summary>
+    public class RepeatingTimer : Timer
+    {
+        public RepeatingTimer(float time, Action onElapsed = null):base(time,onElapsed)
+        {
+        }
+
+        public override bool Tick(float deltaTime)
+        {
+            if (AssessTime(deltaTime))
+            {
+                Reset();
+            }
+
+            return false;
+        }
+    }
+}
--- Assets/Scripts/TowerDefense/Wave/Wave.cs
+++ Assets/Scripts/TowerDefense/Wave/Wave.cs
@@ -1,4 +1,5 @@
 ﻿using Core.Utilities;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,5 +11,33 @@ namespace TowerDefense.Wave
         /// 出怪指令队列
         /// </summary>
         public List<SpawnInstruction> spawnInstructions;
+
+        protected int m_CurrentIndex;
+
+        /// <summary>
+        /// 出怪间隔 (重复计时器)
+        /// </summary>
+        protected RepeatingTimer m_spawnTimer;
+
+        /// <summary>
+        /// 波次完成
+        /// </summary>
+        public event Action waveCompleted;
+
+        /// <summary>
+        /// 波次进度
+        /// </summary>
+        public virtual float progress
+        {
+            get { return (float)(m_CurrentIndex) / spawnInstructions.Count; }
+        }
+
+        /// <summary>
+        /// 初始化波次
+        /// </summary>
+        public virtual void Init()
+        {
+
+        }
     }
 }
